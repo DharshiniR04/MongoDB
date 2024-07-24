8d3972a5c60823b315a7c8a3d2d36e4048f83c09
Dharshini <2112026@nec.edu.in>
2024-07-24 22:47:46 +0530

    Changes in Find

--- Find/playground-2.mongodb.js
+++ Find/playground-2.mongodb.js
@@ -10,8 +10,16 @@ show ('collections')
 //retrives documents from book
 db.book.find().pretty();
 
+//retrives all book's title and rating
+db.book.find({},{title:1,Rating:1});
+
 //retrives the book's data from DB  which has rating lesser than 9
 db.book.find({Rating:{$lt:9}},{title:1,Rating:1}).pretty();
 
+//retrives the book data based on genre(Mystry) or rating(>=8)
+db.book.find({$or: [{genre:"Mystry"},{Rating:{$lt:8}}]}).pretty();
 
+//retrives the book data based on genre(Mystry) and rating(>=8)
+db.book.find({$and: [{genre:"fantasy"},{Rating:{$lt:8}}]}).pretty();
+  
 
